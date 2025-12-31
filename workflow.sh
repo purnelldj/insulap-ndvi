424e235c336b4c7749e03d22eebb942946b4ac1a
dave <davejpurnell@gmail.com>
2025-12-31 17:30:48 +0100

    edit the workflow including output path

--- workflow.sh
+++ workflow.sh
@@ -2,15 +2,19 @@
 
 set -x -e
 
-# two input params: s2 product and bbox
+# input patams: s2prod, BBOX
+# outputs: ndvipng
 
 # s2 product will be automatically saved to 
 S2PRODPATH=/home/worker/workDir/inDir/s2prod
 
-# bbox will be stored in as BBOX
+# bbox will be stored in the following file as BBOX
 source /home/worker/workDir/FSTEP-WPS-INPUT.properties
+
+# output png should be saved at
+OUTPNGPATH=/home/worker/workDir/outDir/ndvipng/ndvi.png
 
 # run the script with the correct paths
 python3 ndvi_calculator.py ${S2PRODPATH}
     --bbox ${BBOX} \
-    --out-png /data/ndvi.png
+    --out-png ${OUTPNGPATH}
