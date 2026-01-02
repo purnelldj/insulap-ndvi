f6396017222bccd5bd3d9e3a7f901cab8040a513
dave <davejpurnell@gmail.com>
2026-01-02 13:25:34 +0100

    mirror example from docs more closely

--- Dockerfile
+++ Dockerfile
@@ -4,20 +4,20 @@ ENV DEBIAN_FRONTEND=noninteractive \
 	PYTHONDONTWRITEBYTECODE=1 \
 	PYTHONUNBUFFERED=1
 
-RUN mkdir -p /home/worker/processor
-WORKDIR /home/worker/processor
-
 # Install pip for the base image's Python.
 RUN apt-get update \
 	&& apt-get install -y --no-install-recommends \
 		python3-pip \
 	&& rm -rf /var/lib/apt/lists/*
 
-COPY requirements.txt /app/requirements.txt
-RUN python3 -m pip install --no-cache-dir --break-system-packages -r /app/requirements.txt
+RUN mkdir -p /home/worker/processor
+
+WORKDIR /home/worker/processor
 
 COPY . /home/worker/processor
 
 RUN chmod +x /home/worker/processor/workflow.sh
+
+RUN python3 -m pip install --no-cache-dir --break-system-packages -r requirements.txt
 
 ENTRYPOINT ["/home/worker/processor/workflow.sh"]
--- workflow.sh
+++ workflow.sh
@@ -2,17 +2,34 @@
 
 set -x -e
 
+WORKFLOW=$(dirname $(readlink -f "$0"))
+WORKER_DIR="/home/worker"
+IN_DIR="${WORKER_DIR}/workDir/inDir"
+OUT_DIR="${WORKER_DIR}/workDir/outDir"
+WPS_PROPS="${WORKER_DIR}/workDir/FSTEP-WPS-INPUT.properties"
+PROC_DIR="${WORKER_DIR}/procDir"
+TIMESTAMP=$(date --utc +%Y%m%d_%H%M%SZ)
+
+# Input parameters available as shell variables
+source ${WPS_PROPS}
+
+# below is all stuff that was in the example but doesn't make sense to me
+# mkdir -p ${IN_DIR}/input
+# mkdir -p ${OUT_DIR}/output
+# cat ${WPS_PROPS}
+# ls -l ${IN_DIR}/input/
+# cp -r ${IN_DIR}/input/* ${OUT_DIR}/output
+# finish
+
 # input patams: s2prod, BBOX
+# BBOX read from WPS_PROPS
 # outputs: ndvipng
 
-# s2 product will be automatically saved to 
-S2PRODPATH=/home/worker/workDir/inDir/s2prod
-
-# bbox will be stored in the following file as BBOX
-source /home/worker/workDir/FSTEP-WPS-INPUT.properties
+# s2 product will in theory be automatically saved to 
+S2PRODPATH=${IN_DIR}/s2prod
 
 # output png should be saved at
-OUTPNGPATH=/home/worker/workDir/outDir/ndvipng/ndvi.png
+OUTPNGPATH=${OUT_DIR}/ndvipng/ndvi.png
 
 # run the script with the correct paths
 python3 ndvi_calculator.py ${S2PRODPATH} \
